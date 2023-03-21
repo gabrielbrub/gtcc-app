9a2f3bfd29e4fa88bd1ef8f83330a118a19ff8ee
gabriel <gabrielbrub@hotmail.com>
2023-03-20 23:55:06 -0300

    navbar clicavel

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,11 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 const Navbar: React.FC = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className='w-screen h-[80px] z-10 bg-zinc-200 top-0 drop-shadow-lg flex items-center'>
-        <div className='px-2 flex justify-between items-center w-full max-w-screen-lg m-auto'>
-            <div className='flex items-center justify-start'>
-                <h1 className='text-3xl font-bold mr-4 sm:text-4xl'>gTCC</h1>
-            </div>
+    <div className='w-screen h-[80px] z-10 bg-zinc-200 top-0 drop-shadow-lg flex items-center'
+    >
+      <div className='px-2 flex justify-between items-center w-full max-w-screen-lg m-auto'>
+        <div className='flex items-center justify-start'>
+          <h1 className='text-3xl font-bold mr-4 sm:text-4xl cursor-pointer'
+            onClick={() => navigate('/deploy')}>gTCC</h1>
         </div>
+      </div>
     </div>
   );
 };
