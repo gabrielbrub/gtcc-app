f812f1ae5c52d835442ac9a049c52ad0f6783250
gabriel <gabrielbrub@hotmail.com>
2023-03-26 00:57:20 -0300

    add contrato a lista via tela usuario

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,17 +1,78 @@
-import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { ChangeEvent, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLocalStorage } from "usehooks-ts";
-import { EthLabel } from "../components/EthLabel";
 import { IpfsButton } from "../components/IpfsButton";
+import { useIPFSContext } from "../components/ipfsContext";
 import Navbar from "../components/navBar";
+import { useEth } from "../components/useEth";
+import { authorAbi } from "../ContractsData";
 import { AuthorDetails } from "../types";
 
 
 export const Home = (): JSX.Element => {
     const [authorDetails, setAuthorDetails] = useState<AuthorDetails[]>([]);
     const [storedValue, setValue] = useLocalStorage<AuthorDetails[]>("@gtcc-author-addresses", []);
+    const [provider, signer, isOnlineETh, signerAddress] = useEth();
     const navigate = useNavigate();
+    const { ipfs, isOnline } = useIPFSContext();
+    const [newAuthorAddress, setNewAuthorAddress] = useState<string>('');
 
+
+    const getMetadataFromIpfs = async (cid: string): Promise<AuthorDetails> => {
+        if (ipfs && isOnline) {
+            const stream = ipfs.cat(cid)
+
+            let concatenatedChunks = new Uint8Array(0)
+            for await (const chunk of stream) {
+                const chunkArray = new Uint8Array(chunk)
+                const newLength = concatenatedChunks.length + chunkArray.length
+                const newConcatenatedChunks = new Uint8Array(newLength)
+                newConcatenatedChunks.set(concatenatedChunks, 0)
+                newConcatenatedChunks.set(chunkArray, concatenatedChunks.length)
+                concatenatedChunks = newConcatenatedChunks
+            }
+            const jsonData: string = new TextDecoder().decode(concatenatedChunks)
+            const jsonObject: Record<string, any> = JSON.parse(jsonData)
+            return jsonObject as AuthorDetails;
+        }
+        throw Error("Not connected to IPFS");
+    }
+
+    const handleAdd = async (authorAddress: string): Promise<void> => {
+        const asyncFunc = async () => {
+            if (isOnline && provider) {
+                try {
+                    const authorContract = new ethers.Contract(authorAddress, authorAbi, provider);
+                    const authorMetadataCid = await authorContract.metadata();
+                    const metadata = await getMetadataFromIpfs(authorMetadataCid);
+    
+                    const newAuthorDetails = {
+                        contractData: {
+                            owner: await authorContract.owner(),
+                            address: authorAddress,
+                            metadata: authorMetadataCid,
+                        },
+                        name: metadata.name,
+                        email: metadata.email,
+                        
+                    }
+                    setValue([...storedValue, newAuthorDetails]);
+                    setAuthorDetails([...authorDetails, newAuthorDetails]);
+                } catch(e) {
+                    alert("Failed to retrieve author. Please check the address and try again.")
+                }
+            } else {
+                alert("Failed to retrieve author. Please check your connection to IPFS and Metamask and try again.");
+            }
+        }
+        if (storedValue.some((contract: AuthorDetails) => contract.contractData.address === authorAddress)) {
+            const owner = storedValue.find((contract: AuthorDetails) => contract.contractData.address === authorAddress);
+            alert("Author contract already on list. The owner is: " + owner?.contractData.owner);
+            return;
+        }
+        asyncFunc();
+    }
 
     return <div>
         <Navbar />
@@ -23,9 +84,11 @@ export const Home = (): JSX.Element => {
             <h1 className='font-semibold mt-4 mb-2'>Authors stored in your browser</h1>
             <form className="flex items-center bg-gray-100 p-4 rounded-md max-w-[50%]" onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
                 e.preventDefault();
+                handleAdd(newAuthorAddress);
             }}>
                 <input type="text" 
                     pattern="^0x[a-fA-F0-9]{40}$"
+                    onChange={(e: ChangeEvent<HTMLInputElement>) => setNewAuthorAddress((e.target as HTMLInputElement).value)}
                     title="Enter a valid Ethereum address"
                     required
                     className="px-4 py-2 w-full rounded-l-md border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
